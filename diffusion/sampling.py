remove computation of likelihood (wrong) + add energy fct(torchani)

diffusion/sampling.py
@@ -16,7 +16,8 @@ from spyrmsd import molecule, graph
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 still_frames = 10
 from diffusion import torus
-from einops import reduce
+from einops import reduce, rearrange, repeat
+from gflownet.energy import Energy
 def try_mmff(mol):
     try:
         AllChem.MMFFOptimizeMoleculeConfs(mol, mmffVariant='MMFF94s')
@@ -94,6 +95,8 @@ def perturb_seeds(data, pdb=None):
 def g(t):
     pass
 
+
+
 def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20, batch_size=32,
            ode=False, likelihood=None, pdb=None, pg_weight_log_0=None, pg_repulsive_weight_log_0=None,
            pg_weight_log_1=None, pg_repulsive_weight_log_1=None, pg_kernel_size_log_0=None,
@@ -106,7 +109,7 @@ def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20,
     sigma_schedule = 10 ** np.linspace(np.log10(sigma_max), np.log10(sigma_min), steps + 1)[:-1]
     eps = 1 / steps
 
-    
+    energy = Energy(oracle='torchani')
 
     if pg_weight_log_0 is not None and pg_weight_log_1 is not None:
         edge_index, edge_mask = conformers[0].edge_index, conformers[0].edge_mask
@@ -155,10 +158,10 @@ def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20,
             except TimeoutException as e:
                 print("Timeout generating with non invariant kernel")
                 pg_invariant = False
-
+    
     for batch_idx, data in enumerate(loader):
-        logit_trajs_forward = torch.zeros(data.num_graphs, len(sigma_schedule))
-        logit_trajs_backward = torch.zeros(data.num_graphs, len(sigma_schedule))
+        logit_pf = torch.zeros(data.num_graphs, len(sigma_schedule))
+        logit_pb = torch.zeros(data.num_graphs, len(sigma_schedule))
 
         dlogp = torch.zeros(data.num_graphs)
         data_gpu = copy.deepcopy(data).to(device)
@@ -183,8 +186,6 @@ def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20,
                     dlogp += -0.5 * g ** 2 * eps * div
             else:
                 perturb = g ** 2 * eps * score + g * np.sqrt(eps) * z
-                div = divergence(model, data, data_gpu, method='full')
-                dlogp += -0.5 * g ** 2 * eps * div
 
             if pg_weight > 0:
                 n = data.num_graphs
@@ -217,17 +218,17 @@ def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20,
                 mean, std = 0.5 * g ** 2 * eps * score + langevin_weight * (0.5 * g ** 2 * eps * score)  ,  langevin_weight * g * np.sqrt(eps)+torch.eye(data_gpu.edge_pred.shape[0])
             else:
                 mean, std = g ** 2 * eps * score, g * np.sqrt(eps)*torch.eye(data_gpu.edge_pred.shape[0])
-            # compute the forward and backward (in gflownet language) transitions logprobs 
-    
+            
+            # compute the forward and backward (in gflownet language) transitions logprobs
             for i in range(data.num_graphs):
                 n_torsion_angles = len(perturb) // data.num_graphs
                 start, end = i*n_torsion_angles, (i+1)*n_torsion_angles
                 # in forward, the new mean is obtained using the score (see above)
-                p_trajs_forward = torus.p(perturb[start:end].cpu().numpy() - mean[start:end].cpu().numpy(), g.cpu().numpy() ) 
-                logit_trajs_forward[i,sigma_idx ] = torch.log(torch.tensor(p_trajs_forward)).sum() 
+                p_trajs_forward = torus.p((perturb - mean)[start:end].cpu().numpy(), g.cpu().numpy() ) 
+                logit_pf[i,sigma_idx ] = torch.log(torch.tensor(p_trajs_forward)).sum() 
                 # in backward, since we are in variance-exploding, f(t)=0. So the mean of the backward kernels is 0
                 p_trajs_backward = torus.p( - perturb[start:end].cpu().numpy() , g.cpu().numpy() ) 
-                logit_trajs_backward[i,sigma_idx ] = torch.log(torch.tensor(p_trajs_backward)).sum() 
+                logit_pb[i,sigma_idx ] = torch.log(torch.tensor(p_trajs_backward)).sum() 
 
             conf_dataset.apply_torsion_and_update_pos(data, perturb.numpy())
             data_gpu.pos = data.pos.to(device)
@@ -243,12 +244,25 @@ def sample(conformers, model, sigma_max=np.pi, sigma_min=0.01 * np.pi, steps=20,
                 conformers[data.idx[i]].dlogp = d.item()
         
         bs = data.num_graphs
-        logit_trajs_forward = reduce(logit_trajs_forward, 'bs steps-> bs', 'sum' )
-        logit_trajs_backward = reduce(logit_trajs_backward, 'bs steps-> bs', 'sum' )
-        # Sanity check. Here we have bs (=32) sampled trajectories for the same smiles molecule. Ins gfn language we can compare logp(x0) = log( \int_{tau ~ x0} pf(tau)/pb(tau) dtau)  ≈ logsumexp(1/bs * (logit_trajs_forward - logit_trajs_backward))
-        print(torch.logsumexp(1/bs * (logit_trajs_forward - logit_trajs_backward), dim=0), dlogp)
+        logit_pf = reduce(logit_pf, 'bs steps-> bs', 'sum' )
+        logit_pb = reduce(logit_pb, 'bs steps-> bs', 'sum' )
+        # Sanity check. Ins gfn language we can compare logp(x0) ≈ logit_pf(tau) - logit_pb(tau), where tau is a trajectory that leads to x0
+        # TODO compute exact likelihood of samples
+        print('ogit_pf - logit_pb', logit_pf - logit_pb)
+        #Computing logrews
+        pos = rearrange(data.pos, '(bs n) d -> bs n d', bs=bs)
+        z = rearrange(data.z, '(bs n) -> bs n', bs=bs)
+        logrews = energy.logrew(z, pos)
+        #Computing vargradloss. 
+        try:
+            assert all(x == data.canonical_smi[0] for x in data.canonical_smi)
+        except:
+            raise ValueError("Vargrad loss should be computed for the same molecule only ! Otherwise we have different Zs, so doesn't make sense")
+        vargrad_quotients = logit_pf - logit_pb - logrews
+        vargrad_loss = torch.var(vargrad_quotients)
+        print('vargrad loss', vargrad_loss)
         breakpoint()
-         
+    
     return conformers
 
 