4a3cca47362c3a3069222ff16722431497104b71
lena-nehale.ezzine <lena-nehale.ezzine@cn-a001.server.mila.quebec>
2024-11-26 09:52:23 -0500

    add doc for modify_conformer, to be parallelized

--- utils/torsion.py
+++ utils/torsion.py
@@ -55,6 +55,17 @@ def get_distance_matrix(pyg_data, mask_edges, mask_rotate):
 
 
 def modify_conformer(pos_input, edge_index, mask_rotate, torsion_updates_input, as_numpy=False):
+    '''
+    Modifies the conformer's 3D coordinates based on the torsion updates.
+    Args:
+        pos_input: (N, 3) tensor of 3D coordinates
+        edge_index: (E, 2) tensor of edge indices
+        mask_rotate: (E, N) tensor of boolean masks indicating which atoms are rotated by each edge
+        torsion_updates_input: (E,) tensor of torsion updates
+        as_numpy: whether to return the output as a numpy array
+    Returns:
+        pos: (N, 3) tensor of modified 3D coordinates
+    '''
     pos = copy.deepcopy(pos_input)
     if type(pos) != np.ndarray: pos = pos.cpu().numpy()
     if type(torsion_updates_input) != np.ndarray: 
